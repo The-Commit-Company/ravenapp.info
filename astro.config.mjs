c9bb2543361c33cd65a32bc3a4c0d7274d0a28ca
Nikhil Kothari <nik.kothari22@live.com>
2023-06-22 20:39:03 +0530

    feat: added github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), sitemap()]
+  integrations: [react(), tailwind(), sitemap()],
+  site: 'https://raven.github.io',
 });
